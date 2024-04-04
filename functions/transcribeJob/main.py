efdca0e161f2cad23a9f1d51207194b545a3e7b5
arvehisa <arvehisa@gmail.com>
2024-04-04 22:35:23 +0900

    test lambda

--- functions/transcribeJob/main.py
+++ functions/transcribeJob/main.py
@@ -4,5 +4,5 @@ def lambda_handler(event, context):
     print('hello')
     return {
         'statusCode': 200,
-        'body': json.dumps("hello worlds!")
+        'body': json.dumps("hello world!")
     }
