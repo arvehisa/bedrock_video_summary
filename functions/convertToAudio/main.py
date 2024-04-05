ed41efeed1f0267a01b91e930512a67936638484
arvehisa <arvehisa@gmail.com>
2024-04-05 09:25:17 +0900

    update convertToAudio

--- functions/convertToAudio/main.py
+++ functions/convertToAudio/main.py
@@ -1,8 +1,26 @@
-import json
+import os
+import boto3
+import subprocess
+from urllib.parse import unquote_plus
+
+s3_client = boto3.client('s3')
 
 def lambda_handler(event, context):
-    print('hello')
-    return {
-        'statusCode': 200,
-        'body': json.dumps("hello world")
-    }
+    source_bucket = event['Records'][0]['s3']['bucket']['name']
+    key = unquote_plus(event['Records'][0]['s3']['object']['key'])
+    
+    download_path = f'/tmp/{key}'
+    output_path = f'/tmp/{key}.m4a'
+
+    output_bucket = 'videosum-audio-output'
+    
+    s3_client.download_file(source_bucket, key, download_path)
+    
+    convert_mp4_to_m4a(download_path, output_path)
+    
+    s3_client.upload_file(output_path, output_bucket, os.path.basename(output_path))
+
+
+def convert_mp4_to_m4a(input_file, output_file):
+    command = ['ffmpeg', '-i', input_file, '-vn', '-acodec', 'copy', output_file]
+    subprocess.run(command, check=True)
